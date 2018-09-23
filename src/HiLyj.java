a5474820bdd6bcf0d76abcc1c8385a074514c1eb
babyL <315921649@qq.com>
2018-09-23 16:49:53 +0800

    测试！！！

--- src/HiLyj.java
+++ src/HiLyj.java
@@ -13,6 +13,7 @@ public class HiLyj {
     * @Date: 2018/9/23 
     */ 
     public static void main(String[] args) {
+        System.out.println("怎么提交不上去啊？？？？");
         System.out.println("你好啊，Lyj同学！");
         System.out.println("66666");
     }
