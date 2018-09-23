85455e1a4e4db94c4ded1ef9c812fd479799050f
babyL <315921649@qq.com>
2018-09-23 16:09:26 +0800

    测试！！！

---
+++ src/HiLyj.java
@@ -0,0 +1,18 @@
+/**
+ * @program: hilyj
+ * @description: 测试着玩玩
+ * @author:
+ * @createDate: 2018-09-23 16:00
+ **/
+public class HiLyj {
+    /** 
+    * @Description:  
+    * @Param:  
+    * @return:  
+    * @Author: babyL 
+    * @Date: 2018/9/23 
+    */ 
+    public static void main(String[] args) {
+        System.out.println("你好啊，Lyj同学！");
+    }
+}
